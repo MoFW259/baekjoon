f14fa9421fc358e728fe66a286f7932ea081c17c
2hoyean <leehy0906166@gmail.com>
2024-07-26 12:21:52 +0900

    [Silver I] Title: 양, Time: 0 ms, Memory: 1188 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.cc
+++ 백준/Silver/3184. 양/양.cc
@@ -1,34 +1,23 @@
 #include <stdio.h>
 #include <algorithm>
-#include <queue>
 using namespace std;
 int n,m;
 char a[251][251];
 int dx[4]={0,1,0,-1};
 int dy[4]={1,0,-1,0};
-int rsheep=0,rwolf=0;
-void bfs(int x,int y){
-	queue<pair <int,int> > q;
-	q.push({x,y});
-	int sheep=0,wolf=0;
+int sheep=0,wolf=0,rsheep=0,rwolf=0;
+void dfs(int x,int y){//dfs
+	if(a[x][y]=='#') return;
 	if(a[x][y]=='v') wolf++;
 	else if(a[x][y]=='o') sheep++;
     a[x][y]='#';
-    while(!q.empty()){
-		int nx=q.front().first,ny=q.front().second;
-	    q.pop();
-    	for(int i=0;i<4;i++){//상하좌우 
-	        int xx=nx+dx[i],yy=ny+dy[i];
-	        if(a[xx][yy]!='#'&&xx>=0&&xx<n&&yy>=0&&yy<m){
-	        	if(a[xx][yy]=='v') wolf++;
-	        	else if(a[xx][yy]=='o') sheep++;
-	        	a[xx][yy]='#';
-	        	q.push({xx,yy});
-			}
+    for(int i=0;i<4;i++){//상하좌우 
+        int xx=x+dx[i],yy=y+dy[i];
+        if(a[xx][yy]!='#'&&xx>=0&&xx<n&&yy>=0&&yy<m){
+        	dfs(xx,yy);
 		}
 	}
-	if(sheep>wolf) rsheep+=sheep;
-	else rwolf+=wolf;
+	return;
 }
 int main(){
 	char c[26];
@@ -38,9 +27,13 @@ int main(){
 		scanf("%s",&a[i]);
 	}for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
+			if(a[i][j]=='#') continue;
 			if(a[i][j]=='o'||a[i][j]=='v'){//#이 나오면 dfs실행 
-				bfs(i,j);
-				
+				sheep=0,wolf=0;
+				dfs(i,j);
+				//printf("%d %d\n",sheep,wolf);
+				if(sheep>wolf) rsheep+=sheep;
+				else rwolf+=wolf;
 			}
 		}
 	}
