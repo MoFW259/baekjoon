b1d94e25598ef798e71282d25255d913662a1f55
2hoyean <leehy0906166@gmail.com>
2025-02-02 16:28:01 +0900

    [Silver V] Title: 삼각형만들기, Time: 780 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2622. 삼각형만들기/삼각형만들기.cc
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.cc
@@ -1,16 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+int min(int a,int b){
+    if(a<b){
+        return a;
+    }
+    return b;
+}
 int main(){
-	int n,r=0,longest;
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++){
-		for(int j=i;j<=n;j++){
-			longest=n-i-j;
-			//printf("%d %d %d\n",i,j,longest);
-			if(j>longest) break;
-			if(i+j>longest){
-				r++;
-			}
-		}
-	}
-	printf("%d",r);
+    int n;
+    int cnt=0;
+    int k;
+    scanf("%d",&n);
+    for(int i=n-2;i>=1;i--){
+        for(int j=min(n-i,i);j>0;j--){
+            k=n-i-j;
+            if(k<=0){
+                continue;
+            }
+            if(k>j){
+                continue;
+            }
+            if(k>i){
+                continue;
+            }
+            if(j>i){
+                continue;
+            }
+            if(i<k+j){
+                //printf("%d %d %d\n",i,j,k);
+                cnt++;
+            }
+            
+        }
+    }
+    printf("%d",cnt);
 }
