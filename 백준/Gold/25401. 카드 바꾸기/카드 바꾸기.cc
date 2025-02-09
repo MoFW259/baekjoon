c0f01a1d2abf89767da144693cc59c7a16aa7138
2hoyean <leehy0906166@gmail.com>
2025-02-09 16:49:34 +0900

    [Gold V] Title: 카드 바꾸기, Time: 488 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.cc
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.cc
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+/*
+5
+2 1 6 9 10
+->2
+*/
 int main(){
     int n;
     int arr[501]={};
@@ -18,50 +22,56 @@ int main(){
     cnt=0;
     
     for(int i=1;i<n;i++){
-        t=arr[i]-arr[i-1];
-        cnt=0;
-        //printf("<%d>",t);
-        for(int j=0;j<n;j++){
-            temp[j]=arr[j];
-        }
-        if(t!=0){
+        for(int k=1;k<n;k++){
+            t=arr[i]-arr[i-k];
+            cnt=0;
+            //printf("<%d>",t);
             for(int j=0;j<n;j++){
-                if(i>j&&i!=j&&i-1!=j){
-                    if(temp[j]!=-(i-j)*t+arr[i]){
-                        temp[j]=-(i-j)*t+arr[i];
-                        cnt++;
-                        //printf("[%d]",j);
+                temp[j]=arr[j];
+            }
+            if(t!=0){
+                for(int j=0;j<n;j++){
+                    if(i!=1&&min<cnt){
+                        break;
                     }
-                }
-                else if(i<j){
-                    if(temp[j]!=(j-i)*t+arr[i]){
-                        temp[j]=(j-i)*t+arr[i];
-                        cnt++;
-                        //printf("[%d]",j);
+                    if(i>j&&i!=j&&i-n!=j){
+                        if(temp[j]!=-(i-j)*(t/k)+arr[i]){
+                            temp[j]=-(i-j)*(t/k)+arr[i];
+                            cnt++;
+                            //printf("[%d]",j);
+                        }
+                    }
+                    else if(i<j){
+                        if(temp[j]!=(j-i)*(t/k)+arr[i]){
+                            temp[j]=(j-i)*(t/k)+arr[i];
+                            cnt++;
+                            //printf("[%d]",j);
+                        }
                     }
                 }
             }
-        }
-        else{
-            for(int j=0;j<n;j++){
-                if(temp[j]!=arr[i]){
-                    temp[j]=arr[i];
-                    cnt++;
+            else{
+                for(int j=0;j<n;j++){
+                    if(temp[j]!=arr[i]){
+                        temp[j]=arr[i];
+                        cnt++;
+                    }
                 }
             }
+            
+            if(i==1){
+                min=cnt;
+            }
+            else if(min>cnt){
+                min=cnt;
+            }
+            /*
+            for(int j=0;j<n;j++){
+                printf("%d ",temp[j]);
+            }
+            printf("(%d)\n",cnt);*/
         }
         
-        if(i==1){
-            min=cnt;
-        }
-        else if(min>cnt){
-            min=cnt;
-        }
-        /*
-        for(int j=0;j<n;j++){
-            printf("%d ",temp[j]);
-        }
-        printf("(%d)\n",cnt);*/
     }
     printf("%d",min);
 }
