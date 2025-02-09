b150c2d7c70ecc5d666384c0eeca11cb767766b6
2hoyean <leehy0906166@gmail.com>
2025-02-09 17:03:31 +0900

    [Silver III] Title: 주유소, Time: 32 ms, Memory: 2556 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,27 +1,28 @@
 #include<stdio.h>
-int main()
-{
-	int n;
-	// a 거리, b 가격 
-	long long int a[100005] = {0, }, b[100005] = {0, }, cost = 0, min = 1000000010;
-	scanf("%d", &n);
-	for(int i = 0; i<n-1; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-	for(int i = 0; i<n; i++)
-	{
-		scanf("%d", &b[i]);
-	}
-	for(int i = 0; i<n-1; i++)
-	{
-		if(b[i] < min)
-		{
-			min = b[i];
-		}
-		cost += min*a[i];
-	}
-	printf("%d", cost);
-	
-	return 0;
+
+int main(){
+    int n;
+    long long int len[100001];
+    long long int city[100001];
+    
+    long long int m=0;
+    long long int now=0;
+    
+    scanf("%d",&n);
+    for(int i=0;i<n-1;i++){
+        scanf("%lld",&len[i]);
+    }
+    for(int i=0;i<n;i++){
+        scanf("%lld",&city[i]);
+    }
+    
+    now=city[0];
+    for(int i=1;i<n;i++){
+        //printf("%lld %lld\n",now,m);
+        m+=now*len[i-1];
+        if(now>city[i]){
+            now=city[i];
+        }
+    }
+    printf("%lld",m);
 }
