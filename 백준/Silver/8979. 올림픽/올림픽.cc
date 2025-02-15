2bc14b39486342d3c62784237da27b356883b8ec
2hoyean <leehy0906166@gmail.com>
2025-02-15 12:43:41 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 1116 KB, Score: 12 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -1,36 +1,44 @@
-#include <stdio.h>
-int country[1001][4];
-int main() {
-	int N, K;
-	scanf("%d %d", &N, &K);
-	int k=0, rank = 1;
-	for (int i = 1; i <= N; i++) {
-		scanf("%d", &k);
-		for (int j = 1; j <= 3; j++) {
-			scanf("%d", &country[k][j]);
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+
+typedef struct medal
+{
+	int c;
+	int m1;
+	int m2;
+	int m3;
+	int s;
+}medal;
+
+bool com(medal asdf, medal b)
+{
+	if(asdf.m1 == b.m1)
+	{
+		if(asdf.m2 == b.m2)
+		{
+			return asdf.m3 > b.m3;
 		}
+		else return asdf.m2 > b.m2;
 	}
-	for (int i = 1; i <= N; i++) {
-		if (i == K) continue;
-		if (country[K][1] < country[i][1]) {
-			rank++;
-		}
-		else if (country[K][1] > country[i][1]) {
-			break;
-		}
-		else {
-			if (country[K][2] < country[i][2]) {
-				rank++;
-			}
-			else if (country[K][2] > country[i][2]) {
-				break;
-			}
-			else {
-				if (country[K][3] < country[i][3]) {
-					rank++;
-				}
-			}
-		}
+	else return asdf.m1 > b.m1;
+}
+
+int main()
+{
+	medal a[1005] = {0, };
+	int n, k;
+	scanf("%d %d", &n, &k);
+	for(int i = 0; i<n; i++)
+	{
+		scanf("%d %d %d %d", &a[i].c, &a[i].m1, &a[i].m2, &a[i].m3);
 	}
-	printf("%d", rank);
+	sort(a, a + n, com);
+	
+	for(int i = 0; i<n; i++)
+	{
+		if(a[i].c == k) printf("%d", i+1);
+	}
+	
+	return 0;
 }
